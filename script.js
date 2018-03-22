c0f813aaf8a2691dd8ae768c9764d5e14cb22a9d
Marcin <marcinsa12@gmail.com>
2018-03-22 17:44:49 +0000

    init

--- script.js
+++ script.js
@@ -1,89 +1,103 @@
-(function(glob) {
-  function checkOrder(key, order = "ascending") {
-    return (a, b) => {
-      const first = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
-      const second = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
-      let comp = 0;
-      if (first > second) {
-        comp = 1;
-      } else if (first < second) {
-        comp = -1;
-      }
-      return order == "descending" ? comp * -1 : comp;
-    };
-  }
+(function(global) {
+	function checkOrder(key, order = "ascending") {
+		return (a, b) => {
+			const first = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+			const second = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
+			let comp = 0;
+			if (first > second) {
+				comp = 1;
+			} else if (first < second) {
+				comp = -1;
+			}
+			return order == "descending" ? comp * -1 : comp;
+		};
+	}
 
-  const items = Vue.component("itemBox", {
-    props: ["content"],
-    template:
-      "<tr>" +
-      "<td>{{content.id}}</td>" +
-      '<td class="longContent">{{content.name}}</td>' +
-      '<td class="longContent">{{content.created_at}}</td>' +
-      "<td>{{content.sell_recurring}}</td>" +
-      "<td>{{content.sell_recurring_incl_vat}}</td>" +
-      "<td>{{content.sell_recurring_vat}}</td>" +
-      "<td>{{content.sell_setup}}</td>" +
-      "<td>{{content.sell_setup_inc_vat}}</td>" +
-      "<td>{{content.sell_setup_vat}}</td>" +
-      '<td class="longContent">{{content.sku}}</td>' +
-      "<td>{{content.vat_rate}}</td>" +
-      "</tr>"
-  });
+	const items = Vue.component("itemRow", {
+		props: ["content"],
+		template:
+			"<tr>" +
+			"<td>{{content.id}}</td>" +
+			'<td class="longContent">{{content.name}}</td>' +
+			'<td class="longContent">{{content.created_at}}</td>' +
+			"<td>{{content.sell_recurring}}</td>" +
+			"<td>{{content.sell_recurring_incl_vat}}</td>" +
+			"<td>{{content.sell_recurring_vat}}</td>" +
+			"<td>{{content.sell_setup}}</td>" +
+			"<td>{{content.sell_setup_inc_vat}}</td>" +
+			"<td>{{content.sell_setup_vat}}</td>" +
+			'<td class="longContent">{{content.sku}}</td>' +
+			"<td>{{content.vat_rate}}</td>" +
+			"</tr>"
+	});
 
-  var myApp = new Vue({
-    el: "#app",
-    components: {},
-    data: {
-      elements: null,
-      sortFlag: "",
-      filterString: ""
-    },
-    template:
-      "<div>" +
-      "<h1>Sortable list</h1>" +
-      '<input v-model="filterString" value="Toolbox">' +
-      "<table>" +
-      "<tr>" +
-      '<th v-on:click="sortTable($event)">id</th>' +
-      '<th v-on:click="sortTable($event)">name</th>' +
-      '<th v-on:click="sortTable($event)">created at</th>' +
-      '<th v-on:click="sortTable($event)">sell recurring</th>' +
-      '<th v-on:click="sortTable($event)">sell recurring inc vat</th>' +
-      '<th v-on:click="sortTable($event)">sell recurring vat</th>' +
-      '<th v-on:click="sortTable($event)">sell setup</th>' +
-      '<th v-on:click="sortTable($event)">sell setup inc vat</th>' +
-      '<th v-on:click="sortTable($event)">sell setup vat</th>' +
-      '<th v-on:click="sortTable($event)">sku</th>' +
-      '<th v-on:click="sortTable($event)">vat rate</th>' +
-      "</tr>" +
-      '<itemBox v-for="item in elements" v-bind:content="item" v-bind:key="item.id"></itemBox>' +
-      "</table>" +
-      "</div>",
-    methods: {
-      sortTable: function(ev) {
-        let sorting = ev.target.innerHTML.split(" ").join("_");
-        myApp.sortFlag == sorting
-          ? myApp.elements.sort(checkOrder(sorting, "descending"))
-          : myApp.elements.sort(checkOrder(sorting));
-        myApp.sortFlag == sorting
-          ? (myApp.sortFlag = "")
-          : (myApp.sortFlag = sorting);
-      }
-    },
-    computed: {
-      filteredData: function(filterString) {
-        return myApp.elements;
-      }
-    }
-  });
+	var myApp = new Vue({
+		el: "#app",
+		components: {items},
+		data: {
+			elements: null,
+			sortFlag: "",
+			filterString: "",
+			filteredResults: null
+		},
+		template:
+			"<div>" +
+			"<h1>Sortable list</h1>" +
+			'<input v-model="filterString" v-on:keyup="filteredData(filterString)">' +
+			"<table>" +
+			"<tr>" +
+			'<th v-on:click="sortTable($event)">id</th>' +
+			'<th v-on:click="sortTable($event)">name</th>' +
+			'<th v-on:click="sortTable($event)">created at</th>' +
+			'<th v-on:click="sortTable($event)">sell recurring</th>' +
+			'<th v-on:click="sortTable($event)">sell recurring inc vat</th>' +
+			'<th v-on:click="sortTable($event)">sell recurring vat</th>' +
+			'<th v-on:click="sortTable($event)">sell setup</th>' +
+			'<th v-on:click="sortTable($event)">sell setup inc vat</th>' +
+			'<th v-on:click="sortTable($event)">sell setup vat</th>' +
+			'<th v-on:click="sortTable($event)">sku</th>' +
+			'<th v-on:click="sortTable($event)">vat rate</th>' +
+			"</tr>" +
+			'<itemRow v-for="item in filteredResults" v-bind:content="item" v-bind:key="item.id"></itemRow>' +
+			"</table>" +
+			"</div>",
+		methods: {
+			sortTable: function(ev) {
+				let sorting = ev.target.innerHTML.split(" ").join("_");
+				myApp.sortFlag == sorting
+					? myApp.filteredResults.sort(checkOrder(sorting, "descending"))
+					: myApp.filteredResults.sort(checkOrder(sorting));
+				myApp.sortFlag == sorting
+					? (myApp.sortFlag = "")
+					: (myApp.sortFlag = sorting);
+			},
+			filteredData: function(filterString) {
+				filterString = filterString.toUpperCase();
+				let filtered = myApp.elements.filter(el => {
+					if (
+						el.name.toUpperCase().indexOf(filterString) > -1 ||
+						el.id.toString().indexOf(filterString) > -1 ||
+						el.sku.toUpperCase().indexOf(filterString) > -1
+					) {
+						return true;
+					}
+				});
+				myApp.filteredResults = filtered;
+				let searchBox = document.querySelector("input");
+				myApp.filteredResults.length === 0
+					? searchBox.classList.add("wrong")
+					: searchBox.classList.remove("wrong");
+			}
+		}
+	});
 
-  fetch("data.json")
-    .then(function(resp) {
-      return resp.json();
-    })
-    .then(function(prom) {
-      myApp.elements = prom.data;
-    });
-  glob.myApp = myApp;
+	fetch("data.json")
+		.then(function(resp) {
+			return resp.json();
+		})
+		.then(function(prom) {
+			myApp.elements = prom.data;
+			myApp.filteredResults = prom.data;
+		});
+	global.myApp = myApp;
 })(window);
